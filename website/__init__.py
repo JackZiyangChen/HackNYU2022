ec1d951778cf8cd31c7e13788044153a37868949
Jack Chen <jackczy1209@gmail.com>
2022-02-26 14:46:17 -0500

    refactored db setup to __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -5,9 +5,17 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+db = SQLAlchemy()  # SQL Alchemy instance
+DB_NAME = 'database.db'
+
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # triple slash is a relative path
+    db.init_app(app)
+
 
 
     from .views import views
@@ -15,5 +23,15 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
 
 
+    from .flaskDB import Id, Post
+    create_database(app)
+
 
     return app
+
+
+
+def create_database(app):
+    if not path.exists('website'+os.sep+DB_NAME):
+        db.create_all(app=app)
+        print('database created!')
