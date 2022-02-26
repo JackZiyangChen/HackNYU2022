9a93b832bfcb571a3c3aeb39a04a2db2940b4f16
Brian Zou <brianzou03@gmail.com>
2022-02-26 14:42:31 -0500

    Comments on installation process

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,9 @@
 import os
 
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask # pip install flask
+from flask_sqlalchemy import SQLAlchemy # pip install flask-sqlalchemy
 from os import path
-from flask_login import LoginManager
+from flask_login import LoginManager # pip install flask_login
 
 
 def create_app():
@@ -14,6 +14,6 @@ def create_app():
 
     app.register_blueprint(views, url_prefix="/")
 
-
+    # http://localhost:5000  - use this to hit in browser
 
     return app
