4b3e83479a20ad99367cb54806265b158f4e8e89
Jack Chen <jackczy1209@gmail.com>
2022-02-26 19:01:47 -0500

    restructure front-end: move css/js/img to static and modified "href" of various <link> tags

--- website/templates/script.js
+++
@@ -1,8 +0,0 @@
-var lettersGrid = document.getElementById('letterGrid')
-var addLetterBtn = document.getElementById('letterBtn')
-
-function displayPrompt() {
-  lettersGrid.classList.add('active')
-}
-
-addLetterBtn.addEventListener('click', () => displayPrompt())
